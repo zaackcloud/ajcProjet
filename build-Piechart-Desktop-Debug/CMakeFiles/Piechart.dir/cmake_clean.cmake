e86c35fb76391ae3c781abfdbaa546f38103f24d
aurelien <aurelien@DebianAjc.myguest.virtualbox.org>
2023-08-01 09:35:36 +0200

    remplissage git

---
+++ build-Piechart-Desktop-Debug/CMakeFiles/Piechart.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Piechart.dir/main.c.o"
+  "CMakeFiles/Piechart.dir/main.c.o.d"
+  "Piechart"
+  "Piechart.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Piechart.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
